981d57d5951647097eebb6d18a38be7e2a632aa5
SebastianBrunhofer <39154004+SebastianBrunhofer@users.noreply.github.com>
2018-05-18 11:57:58 +0200

    Erste Methode (wip)
    Das ist die erste Methode, der Logik nach sollte sie eigentlich funktionieren, nur mit dem gegebenen Testa´case funktioniert sie dann doch nicht.

--- src/TDTree.java
+++ src/TDTree.java
@@ -44,7 +44,6 @@ public class TDTree {
         }
         public void add(TransportNode neu){
             add(new TDTreeNode(neu),true);
-
         }
         public void add(TDTreeNode neu, boolean chk){
             if(chk){ //damit in jeder ebene des Baumes abwechselnd jeweils nur x oder die y koordinate verglichen wird
@@ -105,6 +104,82 @@ public class TDTree {
             }
         }
 
+        public int[] numNodes(double r, double x, double y){
+            return numNodes(x, y, r, true, new int[2]);
+        }
+        public int[] numNodes(double x, double y, double radius, boolean chk, int[] numAPTS) {
+            if (inRadius(x, y, radius)) {
+                if (data.getType() == Type.AIRPORT) {
+                    numAPTS[0]++;
+                }
+                if (data.getType() == Type.TRAINSTATION) {
+                    numAPTS[1]++;
+                }
+            }
+            if (chk) {
+                switch (relToInterval(x, data.getxCoord(), radius)){
+                    case 1:
+                        if (left != null) {
+                            left.numNodes(x, y, radius, !chk, numAPTS);
+                        }
+                        break;
+                    case 0:
+                        if (left != null) {
+                            left.numNodes(x, y, radius, !chk, numAPTS);
+                        }
+                        if (right != null) {
+                            right.numNodes(x, y, radius, !chk, numAPTS);
+                        }
+                        break;
+                    case -1:
+                        if (right != null) {
+                            right.numNodes(x, y, radius, !chk, numAPTS);
+                        }
+                            break;
+                }
+            } else {
+                switch (relToInterval(y, data.getyCoord(), radius)) {
+                    case 1:
+                        if (left != null) {
+                            left.numNodes(x, y, radius, !chk, numAPTS);
+                        }
+                        break;
+                    case 0:
+                        if (left != null) {
+                            left.numNodes(x, y, radius, !chk, numAPTS);
+                        }
+                        if (right != null) {
+                            right.numNodes(x, y, radius, !chk, numAPTS);
+                        }
+                        break;
+                    case -1:
+                        if (right != null) {
+                            right.numNodes(x, y, radius, !chk, numAPTS);
+                        }
+                        break;
+                }
+            }
+            return numAPTS;
+        }
+
+        private boolean inRadius(double x, double y, double radius){
+            return  Math.sqrt(Math.pow(data.getxCoord() - x, 2) + Math.pow(data.getyCoord() - y, 2)) <= radius;
+        }
+        /*
+        return = -1 wenn außerhalb des Intervalls (kleiner)
+        return = 0 wenn in Intervall
+        return = 1 wenn außerhalb des Intervalls (größer)
+         */
+        private int relToInterval(double center, double value, double radius){
+            if (value < center - radius){
+                return -1;
+            } else if (value > center + radius){
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -116,7 +191,7 @@ public class TDTree {
             while(scn.hasNextLine()){
                 //System.out.println(scn.nextLine());
                 TransportNode neu = new TransportNode(scn.nextLine());
-                neu.print();
+                //neu.print();
                 //System.out.println(neu.getName());
                 baum.add(neu);
             }
@@ -124,6 +199,7 @@ public class TDTree {
             System.out.println("File not found!");
             System.exit(1);
         }
-        baum.root.print();
+        //baum.root.print();
+        System.out.println(baum.root.numNodes(100, 5813.29982, 1818.54657)[0]);
     }
 }
